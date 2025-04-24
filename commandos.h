ad45493577a04a078f976202779da6d8c6ad4dfb
Dvoid27 <rubenalves750@gmail.com>
2025-04-24 16:56:30 +0100

    commandos.h adicionado

---
+++ commandos.h
@@ -0,0 +1,13 @@
+#ifndef COMANDOS_H
+#define COMANDOS_H
+
+void gravarJogo();
+void lerJogo();
+void verificarJogo();
+void EstCasas();
+void resolverJogo();
+void DesfazerUltimoComando();
+void SairJogo();
+
+
+#endif
--- main.c
+++ main.c
@@ -6,9 +6,6 @@
 #include "menu.c"
 
 
-
-
-
 int main() {
     mostrar_menu();
     // E depois continuas com o loop do jogo se for opção 1, por exemplo
