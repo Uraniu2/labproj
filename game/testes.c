69c789e1106b3eaf8ec71a8752e1380d9d1afe85
Dvoid27 <rubenalves750@gmail.com>
2025-05-31 11:32:06 +0100

    restaurantes em braga

--- game/testes.c
+++ game/testes.c
@@ -13,7 +13,6 @@ typedef struct {
     int i, j;
 } Posicao;
 
-
 void imprimirTabuleiro(char tabuleiro[TAMANHO][TAMANHO]) {
     for (int i = 0; i < TAMANHO; i++) {
         for (int j = 0; j < TAMANHO; j++) {
@@ -54,10 +53,8 @@ int temDuplicataNaColuna(char tabuleiro[TAMANHO][TAMANHO], int coluna, char letr
 }
 
 int diagonais_riscadas(char tabuleiro[TAMANHO][TAMANHO]) {
-
     for (int k = 0; k < 2 * TAMANHO - 1; k++) {
         int freq[26] = {0};  
-
         for (int i = 0; i < TAMANHO; i++) {
             int j = k - i;
             if (j >= 0 && j < TAMANHO) {
@@ -65,17 +62,15 @@ int diagonais_riscadas(char tabuleiro[TAMANHO][TAMANHO]) {
                 if (c >= 'A' && c <= 'Z') {
                     int idx = c - 'A';
                     if (freq[idx] > 0)
-                        return 0;  
+                        return 0;
                     freq[idx]++;
                 }
             }
         }
     }
 
-
     for (int k = 0; k < 2 * TAMANHO - 1; k++) {
         int freq[26] = {0};
-
         for (int i = 0; i < TAMANHO; i++) {
             int j = k - (TAMANHO - 1 - i);
             if (j >= 0 && j < TAMANHO) {
@@ -83,16 +78,15 @@ int diagonais_riscadas(char tabuleiro[TAMANHO][TAMANHO]) {
                 if (c >= 'A' && c <= 'Z') {
                     int idx = c - 'A';
                     if (freq[idx] > 0)
-                        return 0;  
+                        return 0;
                     freq[idx]++;
                 }
             }
         }
     }
 
-    return 1; 
+    return 1;
 }
-
 
 int vizinho_riscado(char tabuleiro[TAMANHO][TAMANHO], int linhas, int colunas, int i, int j) {
     int di[] = {-1, 1, 0, 0};
@@ -106,36 +100,6 @@ int vizinho_riscado(char tabuleiro[TAMANHO][TAMANHO], int linhas, int colunas, i
         }
     }
     return 0;
-}
-
-int casa_isolada(char tabuleiro[TAMANHO][TAMANHO], int linhas, int colunas, int i, int j_col) {
-
-    int eh_canto = 
-        (i == 0 && j_col == 0) ||  
-        (i == 0 && j_col == colunas - 1) ||  
-        (i == linhas - 1 && j_col == 0) ||  
-        (i == linhas - 1 && j_col == colunas - 1);  
-
-    if (!eh_canto)
-        return 0;
-
-    
-    int di[] = {-1, 1, 0, 0};
-    int dj[] = {0, 0, -1, 1};
-
-    for (int k = 0; k < 4; k++) {
-        int ni = i + di[k];
-        int nj = j_col + dj[k];
-
-        
-        if (ni >= 0 && ni < linhas && nj >= 0 && nj < colunas) {
-            if (tabuleiro[ni][nj] != '#') {
-                return 0;
-            }
-        }
-    }
-
-    return 1; 
 }
 
 int dentroDoTabuleiro(int i, int j) {
@@ -166,7 +130,6 @@ int todasConectadas(char tabuleiro[TAMANHO][TAMANHO]) {
     int encontrou = 0;
     int start_i = -1, start_j = -1;
 
-    
     for (int i = 0; i < TAMANHO && !encontrou; i++) {
         for (int j = 0; j < TAMANHO && !encontrou; j++) {
             if (tabuleiro[i][j] >= 'A' && tabuleiro[i][j] <= 'Z') {
@@ -178,72 +141,35 @@ int todasConectadas(char tabuleiro[TAMANHO][TAMANHO]) {
     }
 
     if (!encontrou)
-        return 1; 
+        return 1;
 
-    
     dfs(tabuleiro, visitado, start_i, start_j);
 
-    
     for (int i = 0; i < TAMANHO; i++) {
         for (int j = 0; j < TAMANHO; j++) {
             if ((tabuleiro[i][j] >= 'A' && tabuleiro[i][j] <= 'Z') && !visitado[i][j])
-                return 0; 
+                return 0;
         }
     }
 
-    return 1; 
+    return 1;
 }
-void minimizarNaoVizinhoDeRiscados(char tabuleiro[TAMANHO][TAMANHO]) {
-    int di[] = {-1, 1, 0, 0};
-    int dj[] = {0, 0, -1, 1};
-
-    for (int i = 0; i < TAMANHO; i++) {
-        for (int j = 0; j < TAMANHO; j++) {
-            
-            if (tabuleiro[i][j] >= 'A' && tabuleiro[i][j] <= 'Z') {
-                int podeMinimizar = 1;
-
-                
-                for (int k = 0; k < 4; k++) {
-                    int ni = i + di[k];
-                    int nj = j + dj[k];
-
-                    
-                    if (dentroDoTabuleiro(ni, nj) && tabuleiro[ni][nj] == '#') {
-                        podeMinimizar = 0;  
-                        break;
-                    }
-                }
-
-
-                if (podeMinimizar) {
-                    tabuleiro[i][j] = tolower(tabuleiro[i][j]);
-                }
-            }
-        }
-    }
-}
-
 
 void aplicarRegras(char tabuleiro[TAMANHO][TAMANHO]) {
     int casasRiscadas[TAMANHO][TAMANHO] = {0};
     Posicao candidatos[100];
     int total = 0;
 
-
     for (int i = 0; i < TAMANHO; i++)
         for (int j = 0; j < TAMANHO; j++)
             pintarBranco(tabuleiro, i, j);
 
-
     for (int i = 0; i < TAMANHO; i++) {
         for (int j = 0; j < TAMANHO; j++) {
             char letra = tabuleiro[i][j];
-
             if ((letra >= 'A' && letra <= 'Z') &&
                 (temDuplicataNaLinha(tabuleiro, i, letra) || temDuplicataNaColuna(tabuleiro, j, letra)) &&
                 !casasRiscadas[i][j]) {
-
                 candidatos[total].i = i;
                 candidatos[total].j = j;
                 total++;
@@ -251,48 +177,74 @@ void aplicarRegras(char tabuleiro[TAMANHO][TAMANHO]) {
         }
     }
 
-
     for (int k = 0; k < total; k++) {
         int i = candidatos[k].i;
         int j = candidatos[k].j;
-
 
         char copia[TAMANHO][TAMANHO];
         for (int x = 0; x < TAMANHO; x++)
             for (int y = 0; y < TAMANHO; y++)
                 copia[x][y] = tabuleiro[x][y];
 
-
         copia[i][j] = '#';
 
         int pode_riscar = 0;
-
 
         if (vizinho_riscado(tabuleiro, TAMANHO, TAMANHO, i, j)) {
             if (diagonais_riscadas(copia)) {
                 pode_riscar = 1;
             }
         } else {
-
             if (todasConectadas(copia)) {
-                
                 pode_riscar = 1;
-                
             }
         }
-
 
         if (pode_riscar) {
             riscar1(tabuleiro, i, j);
             casasRiscadas[i][j] = 1;
         }
     }
-    
-
 }
 
+// 🔍 Verificação final da solução
+void verificarTabuleiroFinal(char tabuleiro[TAMANHO][TAMANHO]) {
+    int valido = 1;
 
+    for (int i = 0; i < TAMANHO; i++) {
+        for (char letra = 'A'; letra <= 'Z'; letra++) {
+            if (temDuplicataNaLinha(tabuleiro, i, letra)) {
+                printf("Letra %c duplicada na linha %d\n", letra, i);
+                valido = 0;
+            }
+        }
+    }
 
+    for (int j = 0; j < TAMANHO; j++) {
+        for (char letra = 'A'; letra <= 'Z'; letra++) {
+            if (temDuplicataNaColuna(tabuleiro, j, letra)) {
+                printf("Letra %c duplicada na coluna %d\n", letra, j);
+                valido = 0;
+            }
+        }
+    }
+
+    if (!diagonais_riscadas(tabuleiro)) {
+        printf("Erro nas diagonais.\n");
+        valido = 0;
+    }
+
+    if (!todasConectadas(tabuleiro)) {
+        printf("Letras não estão conectadas.\n");
+        valido = 0;
+    }
+
+    if (valido) {
+        printf("Tabuleiro final é válido ✅\n");
+    } else {
+        printf("Tabuleiro final contém erros ❌\n");
+    }
+}
 
 int main() {
     char tabuleiro[TAMANHO][TAMANHO] = {
@@ -307,8 +259,12 @@ int main() {
     imprimirTabuleiro(tabuleiro);
 
     aplicarRegras(tabuleiro);
+
     printf("\nTabuleiro após aplicar as regras:\n");
     imprimirTabuleiro(tabuleiro);
+
+    printf("\nVerificação final do tabuleiro:\n");
+    verificarTabuleiroFinal(tabuleiro);
 
     return 0;
 }
